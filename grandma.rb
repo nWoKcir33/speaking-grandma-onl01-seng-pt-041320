718fb863f115f87f7bd59975c143a4914b761ffa
Rick Lim <rick.jason.a.lim@gmail.com>
2020-04-24 17:24:42 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -4,6 +4,7 @@ if phrase == "I LOVE YOU GRANDMA!"
   return "I LOVE YOU TOO PUMPKIN!"
 elsif phrase == phrase.downcase
   return "HUH?! SPEAK UP, SONNY!"
+  
 else phrase == phrase.upcase
   return "NO, NOT SINCE 1938!"
 end
